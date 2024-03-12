fbf87f686f5b9afb4ecc4976225de6201ca5b50a
Taras Machyta <taras.machyta.sa@gmail.com>
2024-03-12 20:23:49 +0200

    Solution

---
+++ src/main/java/jv/springbootsecuritybasics/security/AuthenticationService.java
@@ -0,0 +1,25 @@
+package jv.springbootsecuritybasics.security;
+
+import jv.springbootsecuritybasics.dto.UserLoginRequestDto;
+import jv.springbootsecuritybasics.dto.UserLoginResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(requestDto.email(), requestDto.password())
+        );
+
+        String token = jwtUtil.generateToken(authentication.getName());
+        return new UserLoginResponseDto(token);
+    }
+}
